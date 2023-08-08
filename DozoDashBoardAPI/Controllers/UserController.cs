be007aab99bce4f59f95a5fc38aad33e9a37cd3c
Doruk Gürsey <dorukgursey@gmail.com>
2023-08-08 16:42:53 +0300

    Delete User

--- DozoDashBoardAPI/Controllers/UserController.cs
+++ DozoDashBoardAPI/Controllers/UserController.cs
@@ -1,7 +1,11 @@
-﻿using DozoDashBoard.Models;
+﻿using DozoDashBoard.Data;
+using DozoDashBoard.Extensions;
+using DozoDashBoard.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
 using System.Security.Claims;
 
 namespace DozoDashBoardAPI.Controllers
@@ -10,15 +14,43 @@ namespace DozoDashBoardAPI.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        [HttpGet("GetUser")]
-        [Authorize]
-        public IActionResult GetUser()
+        private readonly DozoDashBoardDbContext _db;
+
+        public UserController(DozoDashBoardDbContext db)
         {
-            var currentUser = GetCurrentUser();
+            _db = db;
+        }
+
+        [HttpGet("GetCurrentUser")]
+        [Authorize]
+        public IActionResult GetCurrentUser()
+        {
+            var currentUser = CurrentUser();
 
             return Ok(currentUser);
         }
-        private UserModel GetCurrentUser()
+        [HttpGet("GetUserList")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<IEnumerable<UserModel>>> GetUserList()
+        {
+            var users = await _db.Users.ToListAsync();
+            return Ok(users);
+        }
+        [HttpDelete("DeleteUser")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteUser(Guid UserId)
+        {
+            var user = await _db.Users.FindAsync(UserId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            _db.Users.Remove(user);
+            await _db.SaveChangesAsync();
+
+            return NoContent();
+        }
+        private UserModel CurrentUser()
         {
 
             var identity = HttpContext.User.Identity as ClaimsIdentity;
